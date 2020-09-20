3f690688551c3e5e1e88df3c0122aa4fa3f1bc80
David Moraes <investtraderrs@gmail.com>
2020-09-20 15:50:59 -0300

    ModDeParametrosREFeOUT

---
+++ ModificadoresDeParametrosREFeOUT/Calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadoresDeParametrosREFeOUT
+{
+    class Calculadora
+    {
+        /*Ao adicionar a palavra REF no parâmetro da função,
+         ela permitirá que a função consiga alterar o valor da
+         variável que foi enviado como parâmetro, pois a variável
+         da função funcionara como uma referência para a variável
+         original, ou seja, ela passará a apontar para a variável
+         original, tornando assim possível alterar seu valor,
+         possibilitando que o resultado da função exemplo
+         seja atribuido a variável original que foi enviada como
+         parâmetro para a operação*/
+
+        public static void Triplicar(ref int x)         {
+            x = x*3;
+        }
+
+        /*Ja o modificador OUT é similar ao REF(faz o parâmetro ser uma referência
+         para a variável original), mas a diferencia é que o OUT não exige que a
+        variável original seja iniciada. E deve ser atribuido a variavel
+        que terá o resultado da função*/
+
+        public static void Triplicar2(int x, out int y) 
+        {
+            y = x * 3;
+        }
+
+    }
+}
---
+++ ModificadoresDeParametrosREFeOUT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ModificadoresDeParametrosREFeOUT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; //A palavra REF exige que a variável seja iniciada, ou seja possua um valor.
+            Calculadora.Triplicar(ref a); //Para funcionar a função com a palavra ref tem que adicionar a palavra ref na variável que será enviada também.
+            Console.WriteLine(a);
+
+            int b = 20; //Variável original que possui o valor para a função.
+            int c; //Variável de Saída, não iniciada que queremos guardar o resultado da função.
+            Calculadora.Triplicar2(b, out c); //Variável "b" que contém o valor que queremos enviar para a operação, e a variável "c" será a variável que receberá o valor resultado a função.
+            Console.WriteLine(c);
+
+            /*
+             CONSIDERAÇÕES SOBRE "REF" e "OUT":
+            - DIFERENÇA:
+            A variável passada como parâmetro "ref" DEVE ter sido iniciada.
+            A variável passada como parâmetro "out" não precisa ter sido iniciada.
+
+            - CONCLUSÃO:
+            Ambos são muito similares, mas "ref" é uma forma de fazer o compilador
+            obrigar o usuário a iniciar a variável.
+
+            - NOTA:
+            Ambos são considerados "code smells" (design ruim) e deve ser evitados.
+
+             */
+        }
+    }
+}
